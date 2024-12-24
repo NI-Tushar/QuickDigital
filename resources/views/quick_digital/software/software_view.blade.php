ba031cda94e1e4b9ad5c00865ab0c4f78885c26f
unknown <nitushar06@gmail.com>
2024-12-24 18:48:13 +0600

    after removing the download software view page theme

--- resources/views/quick_digital/software/software_view.blade.php
+++ resources/views/quick_digital/software/software_view.blade.php
@@ -1,6 +1,73 @@
 @extends('quick_digital.layout.layout')
 @section('content')
+<link rel="stylesheet" href="{{ url ('front/styles/software.css') }}">
 
-<h1>software</h1>
+<div class="software_section">
+    <div class="centered_list">
+        <div class="soft_heading">
+              <h3>ম্যানেজমেন্ট সফটওয়্যার লিস্টঃ</h3>
+          </div>
+          <ol>
+              <li>Apartment Visitors Management System</li>
+              <li>ART Gallery Management System</li>
+              <li>Auto/Taxi Stand Management System</li>
+              <li>Bank Locker Management System</li>
+              <li>Beauty Parlour Management System</li>
+              <li>Blood Bank & Donor Management System</li>
+              <li>BP Monitoring Management System</li>
+              <li>Bus Pass Management System</li>
+              <li>Car Rental System</li>
+              <li>Car Washing Management System</li>
+              <li>Client Management System</li>
+              <li>Company Visitor Management System</li>
+              <li>Complaint Management System</li>
+              <li>Covid 19 Testing Management System</li>
+              <li>Curfew e-Pass Management System</li>
+              <li>Cyber Café Management System</li>
+              <li>Daily Expense Tracker System</li>
+              <li>Dairy Farm Shop Management System</li>
+              <li>Directory Management System</li>
+              <li>Doctor Appointment Management System</li>
+              <li>Emergency Ambulance Hiring Portal</li>
+              <li>Employee Leaves Management System</li>
+              <li>Employee Record Management System</li>
+              <li>GYM Management System</li>
+              <li>Hospital Management System</li>
+              <li>Hostel Management System</li>
+              <li>Local Services Search Engine Management System</li>
+              <li>Maid Hiring Management System</li>
+              <li>Medical Card Generation System</li>
+              <li>Men Salon Management System</li>
+              <li>News Portal</li>
+              <li>Nipash Virus - Testing Management System</li>
+              <li>Old Age Home Management System</li>
+              <li>Online Banquet Booking System</li>
+              <li>Online Birth Certificate System</li>
+              <li>Online Course Registration</li>
+              <li>Online Fire Reporting System</li>
+              <li>Online Library Management System</li>
+              <li>Online Marriage Registration System</li>
+              <li>Online Nurse Hiring System</li>
+              <li>Online Security Guards Hiring System</li>
+              <li>Online Shopping Portal</li>
+              <li>Park Ticketing Management System</li>
+              <li>Pre-School Enrollment System</li>
+              <li>Rail Pass Management System</li>
+              <li>Restaurant Table Booking System</li>
+              <li>Small CRM</li>
+              <li>Student Management System</li>
+              <li>Student Record System</li>
+              <li>Student Result Management System</li>
+              <li>Student Study Center Management System</li>
+              <li>Teacher Subject Allocation System</li>
+              <li>Teachers Record Management System</li>
+              <li>Tourism Management System</li>
+              <li>Vehicle Parking Management System</li>
+              <li>Vehicle Record System</li>
+              <li>Zoo Management System</li>
+              <li>CRM</li>
+          </ol>
+        </div>
+    </div>
 
 @endsection
