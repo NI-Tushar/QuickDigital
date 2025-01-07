c8e26250aeb838c03708f07e9072679c987c0a61
developerbiplob <developerbiplob.bd@gmail.com>
2025-01-07 17:55:41 +0600

    Admin Dashboard Update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -39,7 +39,7 @@
                                                 <i class="icon p-1 icon-pie-chart customize-icon font-large-2 p-1"></i>
                                             </span>
                                             <div class="stats-amount mr-3">
-                                                <h3 class="heading-text text-bold-600">{{ 0 }}</h3>
+                                                <h3 class="heading-text text-bold-600">{{ $customerOrders }}</h3>
                                                 <p class="sub-heading">Customer Orders</p>
                                             </div>
                                         </div>
@@ -63,6 +63,63 @@
                                             <div class="stats-amount mr-3">
                                                 <h3 class="heading-text text-bold-600">{{ $customerCount }}</h3>
                                                 <p class="sub-heading">Customers</p>
+                                            </div>
+                                            {{-- <span class="inc-dec-percentage">
+                                                <small class="danger"><i class="fa fa-long-arrow-down"></i> 13.6%</small>
+                                            </span> --}}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-lg-6 col-xl-3 col-sm-6 col-12">
+                                        <a href="{{ route('admin.affiliate.order') }}"
+                                            class="d-flex align-items-start mb-sm-1 mb-xl-0 border-right-blue-grey border-right-lighten-5">
+                                            <span class="card-icon primary d-flex justify-content-center mr-3">
+                                                <i class="icon p-1 icon-bar-chart customize-icon font-large-2 p-1"></i>
+                                            </span>
+                                            <div class="stats-amount mr-3">
+                                                <h3 class="heading-text text-bold-600">{{ $affiliatorOrderAmount }}</h3>
+                                                <p class="sub-heading">Affiliators Received Amount</p>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div class="col-lg-6 col-xl-3 col-sm-6 col-12">
+                                        <div
+                                            class="d-flex align-items-start mb-sm-1 mb-xl-0 border-right-blue-grey border-right-lighten-5">
+                                            <span class="card-icon danger d-flex justify-content-center mr-3">
+                                                <i class="icon p-1 icon-pie-chart customize-icon font-large-2 p-1"></i>
+                                            </span>
+                                            <div class="stats-amount mr-3">
+                                                <h3 class="heading-text text-bold-600">{{ $customerOrderAmount }}</h3>
+                                                <p class="sub-heading">Customers Received Amount</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-xl-3 col-sm-6 col-12">
+                                        <div class="d-flex align-items-start border-right-blue-grey border-right-lighten-5">
+                                            <span class="card-icon warning d-flex justify-content-center mr-3">
+                                                <i class="icon p-1 icon-user customize-icon font-large-2 p-1"></i>
+                                            </span>
+                                            <div class="stats-amount mr-3">
+                                                <h3 class="heading-text text-bold-600">{{ $CommissionPaid }}</h3>
+                                                <p class="sub-heading">Commission Paid</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-xl-3 col-sm-6 col-12">
+                                        <div class="d-flex align-items-start">
+                                            <span class="card-icon warning d-flex justify-content-center mr-3">
+                                                <i class="icon p-1 icon-user customize-icon font-large-2 p-1"></i>
+                                            </span>
+                                            <div class="stats-amount mr-3">
+                                                <h3 class="heading-text text-bold-600">{{ $CommissionPending }}</h3>
+                                                <p class="sub-heading">Commission Painding</p>
                                             </div>
                                             {{-- <span class="inc-dec-percentage">
                                                 <small class="danger"><i class="fa fa-long-arrow-down"></i> 13.6%</small>
